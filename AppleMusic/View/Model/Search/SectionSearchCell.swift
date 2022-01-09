e0b1accf31bd249c0b0051d565173fb072cd93a6
Kirill Ramenskii <89827577+KRamenskii@users.noreply.github.com>
2022-01-09 12:40:25 +0700

    Create DataCells for search page & add IconImage.

--- AppleMusic/View/Model/Search/SectionSearchCell.swift
+++ AppleMusic/View/Model/Search/SectionSearchCell.swift
@@ -9,25 +9,26 @@ import Foundation
 import SwiftUI
 
 struct SearchSectionCell {
+    let id: Int
     let title: String
     let nameIconImage: String
 }
 
 extension SearchSectionCell {
     static var searchCollection = [
-        SearchSectionCell(title: "Новый год", nameIconImage: "picture1"),
-        SearchSectionCell(title: "Поп на русском", nameIconImage: "picture2"),
-        SearchSectionCell(title: "Поп", nameIconImage: "picture3"),
-        SearchSectionCell(title: "Хип-хоп на русском", nameIconImage: "picture4"),
-        SearchSectionCell(title: "Пространственное звучание", nameIconImage: "picture5"),
-        SearchSectionCell(title: "Хиты", nameIconImage: "picture6"),
-        SearchSectionCell(title: "Хорошее настроение", nameIconImage: "picture7"),
-        SearchSectionCell(title: "Хиты на русском", nameIconImage: "picture8"),
-        SearchSectionCell(title: "Рок на русском", nameIconImage: "picture9"),
-        SearchSectionCell(title: "Чарты", nameIconImage: "picture10"),
-        SearchSectionCell(title: "DJ-миксы", nameIconImage: "picture11"),
-        SearchSectionCell(title: "Восход", nameIconImage: "picture12"),
-        SearchSectionCell(title: "Хип-хоп", nameIconImage: "picture13"),
-        SearchSectionCell(title: "С любовью, Apple music", nameIconImage: "picture14")
+        SearchSectionCell(id: 1, title: "Новый год", nameIconImage: "picture1"),
+        SearchSectionCell(id: 2, title: "Поп на русском", nameIconImage: "picture2"),
+        SearchSectionCell(id: 3, title: "Поп", nameIconImage: "picture3"),
+        SearchSectionCell(id: 4, title: "Хип-хоп на русском", nameIconImage: "picture4"),
+        SearchSectionCell(id: 5, title: "Пространственное звучание", nameIconImage: "picture5"),
+        SearchSectionCell(id: 6, title: "Хиты", nameIconImage: "picture6"),
+        SearchSectionCell(id: 7, title: "Хорошее настроение", nameIconImage: "picture7"),
+        SearchSectionCell(id: 8, title: "Хиты на русском", nameIconImage: "picture8"),
+        SearchSectionCell(id: 9, title: "Рок на русском", nameIconImage: "picture9"),
+        SearchSectionCell(id: 10, title: "Чарты", nameIconImage: "picture10"),
+        SearchSectionCell(id: 11, title: "DJ-миксы", nameIconImage: "picture11"),
+        SearchSectionCell(id: 12, title: "Восход", nameIconImage: "picture12"),
+        SearchSectionCell(id: 13, title: "Хип-хоп", nameIconImage: "picture13"),
+        SearchSectionCell(id: 14, title: "С любовью, Apple music", nameIconImage: "picture14")
     ]
 }
